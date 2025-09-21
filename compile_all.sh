42b318a426eebcafc3b1d405f3cbda9fbf37ab65
Jackz <me@jackz.me>
2025-09-21 18:44:44 -0500

    create out folder, and use it in compile

--- compile_all.sh
+++ compile_all.sh
@@ -5,7 +5,8 @@ fi
 if [ -z "$SPCOMP_OUT_FOLDER"]; then
     SPCOMP_OUT_FOLDER="plugins"
 fi
+mkdir -p "$SPCOMP_OUT_FOLDER" || true
 for file in scripting/*.sp; do 
     out_filename=$(basename "${file%.*}.smx")
-    $SPCOMP_PATH -i scripting/include $file -o temp/$out_filename 
+    $SPCOMP_PATH -i scripting/include "$file" -o "$SPCOMP_OUT_FOLDER"/"$out_filename" 
 done
